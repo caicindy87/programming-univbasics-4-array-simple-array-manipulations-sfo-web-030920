a1999c3d0e76111e22c167eb43b67e021d193faa
Cindy Cai <caicindy87@gmail.com>
2020-02-22 01:44:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,3 +12,10 @@ def using_pop(array)
   removed_element = array.pop
   removed_element
 end
+
+def pop_with_args(array)
+  first_removed_element = array.pop
+  second_removed_element = array.pop
+  first_removed_element
+  second_removed_element
+end
